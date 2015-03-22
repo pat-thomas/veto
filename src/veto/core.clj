8305b0d846d442820395c15c18ef1326a7e440ae
Pat Thomas <pthomas@relaynetwork.com>
2015-03-21 22:18:38 -0400

    Copy over type definition functionality.

--- src/veto/core.clj
+++ src/veto/core.clj
@@ -1,6 +1,25 @@
-(ns veto.core)
+(ns veto.core
+  (:require [clojure.walk :as walk]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^{:private true} type-registry (atom {}))
+(def ^{:dynamic true} *current-type* nil)
+
+(defn needs-keys
+  [& ks]
+  (reduce (fn [acc k]
+            (assoc acc k nil))
+          {}
+          ks))
+
+(defn compute-namespace-lookup-key
+  ([]
+   (-> *ns* .getName keyword))
+  ([^clojure.lang.Symbol type-name]
+   (-> *ns* .getName (str "/" type-name) keyword)))
+
+(defmacro make-type
+  [^clojure.lang.Symbol type-name ^java.util.Map type-map]
+  (swap! type-registry
+         assoc
+         (compute-namespace-lookup-key type-name)
+         (eval type-map)))
